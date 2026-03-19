8c11d0ad5a6575c2e4483ec7e9fab96cc57075a1
Tlekk <yai57406@gmail.com>
2026-03-19 21:09:31 +0700

    netflix pro ui

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 const movies = [
   {
     title: "Fast Action",
-    category: " Bball",
-    thumbnail: "https://images.unsplash.com/photo-1524985069026-dd778a71c7b4",
-    video: "https://drive.google.com/uc?export=download&id=1WFCoSWozt4vngbNO4tKQQw-9rytk3qA5",
+    category: "Bball",
+    thumbnail: "123.jpg",
+    video: "https://drive.google.com/uc?export=download&id=1DgPjq5xlNzYMJjRRJikICaSrbeIIzRr9",
     preview: "https://www.w3schools.com/html/mov_bbb.mp4"
   },
   {
