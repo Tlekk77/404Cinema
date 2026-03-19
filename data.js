44365327aa56ab10f1ed924e5d0461d91b10f818
Tlekk <yai57406@gmail.com>
2026-03-19 20:40:01 +0700

    fix full netflix ui

---
+++ data.js
@@ -0,0 +1,16 @@
+const movies = [
+  {
+    title: "Fast Action",
+    category: "action",
+    thumbnail: "https://images.unsplash.com/photo-1524985069026-dd778a71c7b4",
+    video: "https://drive.google.com/uc?export=download&id=1WFCoSWozt4vngbNO4tKQQw-9rytk3qA5",
+    preview: "https://www.w3schools.com/html/mov_bbb.mp4"
+  },
+  {
+    title: "Daily Vlog",
+    category: "vlogs",
+    thumbnail: "https://images.unsplash.com/photo-1497032205916-ac775f0649ae",
+    video: "https://drive.google.com/uc?export=download&id=1WFCoSWozt4vngbNO4tKQQw-9rytk3qA5",
+    preview: "https://www.w3schools.com/html/movie.mp4"
+  }
+];
